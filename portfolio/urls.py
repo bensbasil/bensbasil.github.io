6f009e320c360c5f818a7b23f5bbd48d01ea4245
bensbasil <bensdbasil@gmail.com>
2026-02-28 15:43:43 +0530

    .

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,14 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from app import views  # Import your views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('', include('app.urls')), # This looks for the urls.py we just created in 'app'
+    path('', include('app.urls')), # This delegates EVERYTHING to your app's urls.py
 ]
 
-# This allows Django to serve media files (images) during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

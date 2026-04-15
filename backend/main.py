9243cb752e42e3e3af6650db28d08a1b41d477e5
EC2 Default User <ec2-user@ip-172-31-16-93.ec2.internal>
2026-04-15 08:46:40 +0000

    fixed merge conflict

--- backend/main.py
+++ backend/main.py
@@ -84,10 +84,7 @@ Instrumentator().instrument(app).expose(app)
 def root():
     return {"message": "API is running"}
 
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 @app.on_event("startup")
 def startup_event():
     start_scheduler()
